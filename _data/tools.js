e6668b2f7f582f7a1751d778ac5789726b740382
tudormunteanu <tudor@mowowstudios.com>
2024-11-21 15:05:42 +0000

    add the data file

--- _data/tools.js
+++ _data/tools.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const { parse } = require('csv-parse/sync');
 
 module.exports = function() {
-    const csvContent = fs.readFileSync('../ml-development-cycle-step.csv', 'utf-8');
+    const csvContent = fs.readFileSync('./ml-development-cycle-step.csv', 'utf-8');
     return parse(csvContent, {
         columns: true,
         skip_empty_lines: true
